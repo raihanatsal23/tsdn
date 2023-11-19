33d8c88b71d8f17f6838d9c0ed1c71a3a1d93bb6
Raihan Atsal Hafizh <101855324+raihanatsal23@users.noreply.github.com>
2023-11-20 00:51:13 +0700

    tsdn.py

--- tsdn.py
+++ tsdn.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from scipy.stats import zscore
+import seaborn as sns
 
 # Baca data
 df_viewing = pd.read_csv('All_ViewingActivity.csv')
@@ -11,8 +17,6 @@ df_viewing['Duration_seconds'] = pd.to_timedelta(df_viewing['Duration']).dt.tota
 df_viewing['Start Time'] = pd.to_datetime(df_viewing['Start Time'])
 df_viewing = df_viewing.sort_values(by='Start Time')
 df_viewing['Duration'] = pd.to_timedelta(df_viewing['Duration'])
-
-# Corrected line: Convert 'Duration' to timedelta before adding to 'Start Time'
 df_viewing['End Time'] = df_viewing['Start Time'] + df_viewing['Duration']
 
 # Calculate device usage count for each user
@@ -33,7 +37,7 @@ df_viewing['is_Possibly_Sharing'] = (df_viewing['User_Device_Count'] > 2) & (df_
 
 # Visualisasi interaktif dengan Streamlit
 def interactive_visualization(df):
-    st.title('User Viewing Activity Summary')
+    st.title('Visualization User Viewing Activity')
 
     # Sidebar untuk pemilihan opsi
     selected_profile = st.sidebar.selectbox('Select Profile Name:', df['Profile Name'].unique())
@@ -41,6 +45,13 @@ def interactive_visualization(df):
 
     # Filter data berdasarkan opsi yang dipilih
     filtered_df = df[(df['Profile Name'] == selected_profile) & (df['Device Type'] == selected_device)]
+
+    # Visualisasi Scatter Plot
+    st.subheader('Device Type by Profile Name with Start Time')
+    fig, ax = plt.subplots(figsize=(16, 10))
+    sns.scatterplot(x='Start Time', y='Profile Name', hue='Device Type', data=filtered_df, palette='viridis', ax=ax)
+    plt.title('Device Type by Profile Name with Start Time')
+    st.pyplot(fig)
 
     # Display summary information in text format
     st.subheader('Summary Information:')
@@ -52,13 +63,6 @@ def interactive_visualization(df):
     # Display detailed information in table format
     st.subheader('Detailed Information:')
     st.table(filtered_df[['Start Time', 'Device Type', 'Country', 'Duration_seconds', 'is_Possibly_Sharing']])
-
-    # Visualisasi Scatter Plot
-    st.subheader('Device Type by Profile Name with Start Time')
-    fig, ax = plt.subplots(figsize=(16, 10))
-    sns.scatterplot(x='Start Time', y='Profile Name', hue='Device Type', data=filtered_df, palette='viridis', ax=ax)
-    plt.title('Device Type by Profile Name with Start Time')
-    st.pyplot(fig)
 
 # Panggil fungsi visualisasi interaktif
 interactive_visualization(df_viewing)
