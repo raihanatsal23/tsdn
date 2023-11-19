6e29cd2083ddabeac5d3f47364b6fd6ba93c8ba1
Raihan Atsal Hafizh <101855324+raihanatsal23@users.noreply.github.com>
2023-11-19 22:56:58 +0700

    tsdn.py

--- tsdn.py
+++ tsdn.py
@@ -12,21 +12,24 @@ df_profiles = pd.read_csv('All_Profiles.csv')
 df_search = pd.read_csv('All_SearchHistory.csv')
 df_viewing = pd.read_csv('All_ViewingActivity.csv')
 
+# Convert Start Time to datetime
+df_viewing['Start Time'] = pd.to_datetime(df_viewing['Start Time'])
+
 # Create Bokeh plot
-def create_interactive_plot(df_viewing):
+def create_interactive_plot(df):
     p = figure(plot_width=800, plot_height=500, title='Device Type by Profile Name with Start Time',
                x_axis_label='Start Time', y_axis_label='Profile Name')
 
     # Get unique Profile Names
-    profile_names = df_viewing['Profile Name'].unique()
+    profile_names = df['Profile Name'].unique()
 
     # Assign a color to each unique Device Type using Viridis color palette
-    colors = Viridis[len(df_viewing['Device Type'].unique())]
+    colors = Viridis[len(df['Device Type'].unique())]
 
     legend_items = []
 
-    for i, device_type in enumerate(df_viewing['Device Type'].unique()):
-        source = ColumnDataSource(df_viewing[df_viewing['Device Type'] == device_type])
+    for i, device_type in enumerate(df['Device Type'].unique()):
+        source = ColumnDataSource(df[df['Device Type'] == device_type])
 
         # Scatter plot
         scatter = p.scatter(x='Start Time', y='Profile Name', source=source,
@@ -63,7 +66,12 @@ def main():
     start_date = st.sidebar.date_input("Select start date", min_value=min_start_time, max_value=max_start_time)
     end_date = st.sidebar.date_input("Select end date", min_value=start_date, max_value=max_start_time)
 
-    filtered_df = df_viewing[(df_viewing['Start Time'] >= start_date) & (df_viewing['Start Time'] <= end_date)]
+    # Ensure that start_date and end_date are datetime objects
+    start_date = datetime.combine(start_date, datetime.min.time())
+    end_date = datetime.combine(end_date, datetime.min.time())
+
+    # Filter the DataFrame
+    filtered_df = df_viewing.loc[(df_viewing['Start Time'] >= start_date) & (df_viewing['Start Time'] <= end_date)]
 
     # Create Bokeh plot
     bokeh_plot = create_interactive_plot(filtered_df)
