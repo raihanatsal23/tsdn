15e0d147c23e5975ea39735bdb84f566e048cf4f
Raihan Atsal Hafizh <101855324+raihanatsal23@users.noreply.github.com>
2023-11-20 01:35:35 +0700

    tsdn.py

--- tsdn.py
+++ tsdn.py
@@ -46,12 +46,8 @@ def interactive_visualization(df):
     # Filter data berdasarkan opsi yang dipilih
     filtered_df = df[(df['Profile Name'] == selected_profile) & (df['Device Type'] == selected_device)]
 
-    temp = filtered_df.loc['is_Possibly_Sharing'].values
-    if temp[0] == True:
-        st.subheader(f'{selected_profile} indicated for account sharing!')
-    else:
-        st.subheader(f'{selected_profile} may be safe.')
-    
+    st.subheader(f'{selected_profile} may be sharing the account!')
+
     # Visualisasi Scatter Plot
     st.subheader('Device Type by Profile Name with Start Time')
     fig, ax = plt.subplots(figsize=(16, 10))
